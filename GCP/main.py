6405c6869316bfbad9e8efe049bf83dcd1d29712
mikako-shirai <mkkshirai@gmail.com>
2021-09-15 05:50:33 +0900

    [update] update main.py

--- GCP/main.py
+++ GCP/main.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template, request, redirect, abort, url_for
+
+from utils import generate_key, get_date, URL_check, key_check, date_check
+
 import random, string
 import datetime
 from dateutil.relativedelta import relativedelta
@@ -15,47 +18,6 @@ GCP_URL = 'https://short-321807.an.r.appspot.com/'
 keywords = ['custom', 'expiration', 'analysis', 'link', '404', 'error', 'cron', \
             'index', 'index_exp', 'custom_exp', 'result', 'selector']
 months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
-
-# -----------------------------------------------------------------------------------
-
-def generate_key(key_length):
-    letters = [random.choice(string.ascii_letters + string.digits) for i in range(key_length)]
-    return ''.join(letters)
-
-def get_date():
-    dateNow = datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=9)))
-    string = dateNow.strftime('%Y%m')
-    year = int(string[:4])
-    month = int(string[4:])
-    if month >= 7:
-        years = [str(year), str(year + 1)]
-    else:
-        years = [str(year)]
-    return years
-
-def URL_check(URL):
-    validFormat = "https?://[\w/:%#\$&\?\(\)~\.=\+\-]+"
-    return True if re.match(validFormat, URL) else False
-
-def key_check(customKey):
-    if len(customKey) < 6 or len(customKey) > 30:
-        return False
-    invalid = '/ \\'
-    for char in customKey:
-        if char in invalid:
-            return False
-    return True
-
-def date_check(dateSet):
-    dateNow = datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=9)))
-    date6months = dateNow + relativedelta(months=+6, minutes=-1)
-    try:
-        expirationDate = datetime.datetime.strptime(dateSet, '%Y/%m/%d %H:%M:%S%z')
-    except ValueError:
-        return False
-    if expirationDate < dateNow or expirationDate > date6months:
-        return False
-    return True
 
 # -----------------------------------------------------------------------------------
 
