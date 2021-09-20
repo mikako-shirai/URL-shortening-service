626e9f9a0930f4a0e344977ea6a7923e87a33928
mikako-shirai <mkkshirai@gmail.com>
2021-09-20 17:58:02 +0900

    [update] update test file name

--- GCP/tests/test_models.py
+++ GCP/tests/test_models.py
@@ -1,23 +1,10 @@
-from URLshortener.views import short_link, custom_link, short_expiration, custom_expiration, link_analysis, URL_redirect
+from URLshortener.models import *
 
 import unittest
 from unittest.mock import PropertyMock, MagicMock, patch
-import random, string
-import datetime
-from dateutil.relativedelta import relativedelta
-import re
-import os
-from google.cloud import firestore
 
 
-# app = Flask(__name__)
 db = MagicMock()
-
-key_length = 5
-GCP_URL = 'https://short-321807.an.r.appspot.com/'
-keywords = ['custom', 'expiration', 'analysis', 'link', '404', 'error', 'cron', \
-            'index', 'index_exp', 'custom_exp', 'result', 'selector']
-months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 
 # -----------------------------------------------------------------------------------
 
@@ -40,10 +27,10 @@ class TestMain(unittest.TestCase):
     # test class of main.py
 
     def setUpClass():
-        print('============================== test_main START ===============================')
+        print('============================== test_models START ===============================')
  
     def tearDownClass():
-        print('=============================== test_main END ================================')
+        print('=============================== test_models END ================================')
  
     # def setUp(self):
     #     print(' before each test ')
